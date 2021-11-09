b931c3df82bacfc1e91ac88fddd31330fba9f0e8
spaneco <jlaza@spaneco.com>
2021-11-09 08:16:17 +0100

    added new improvements

--- src/SpanecoScrollUp.svelte
+++ src/SpanecoScrollUp.svelte
@@ -1,14 +1,22 @@
 <script lang="ts">
 	import { onMount } from 'svelte';
 
-	export let showOnPx = 150;
-	let hidden = true;
+	export let showOnPx: number = 150;
+	export let clricon: string = '#fff';
+	export let clrbg: string = '#45A1B0';
+	export let width: number = 15;
+	export let radius: string = '0';
+
+	let hidden: boolean = true;
+
+	$:position = $$props.left === undefined ?  0 : 1;
+	$:radius = $$props.round === undefined ?  radius : '50%';
 
 	function scrollContainer() {
 		return document.documentElement || document.body;
 	}
 
-	function handleOnScroll() {
+	function handleOnScroll(): void {
 		if (!scrollContainer()) {
 			return;
 		}
@@ -20,7 +28,7 @@
 		}
 	}
 
-	function scrollToTop() {
+	function scrollToTop(): void {
 		scrollContainer().scrollTo({
 			top: 0,
 			behavior: "smooth"
@@ -37,18 +45,21 @@
 
 <svelte:window on:scroll="{handleOnScroll}" />
 
-<button class="spaneco-scrollup" class:spaneco-scrollup--visible={!hidden} aria-hidden="true" tabindex="-1" on:click={scrollToTop}>
-	<div class="spaneco-scrollup__content">
-		<svg class="spaneco-scrollup__icon" viewBox="0 0 17 10">
-			<path fill="currentColor" d="M14.692 9.63a1.211 1.211 0 101.712-1.715L9.602 1.121a1.211 1.211 0 00-1.714.002L1.113 7.917a1.211 1.211 0 101.716 1.71l5.919-5.935 5.944 5.937z"></path>
+<button class="btn" class:is-visible={!hidden} aria-hidden="true" tabindex="-1" on:click={scrollToTop} style="--position: {position};">
+	<div class="content" style="--clr-icon: {clricon}; --clr-bg: {clrbg}; --width: {`${width}px`}; --radius: {`${radius}`};">
+		<svg class="icon" viewBox="0 0 17 10">
+			<path d="M14.692 9.63a1.211 1.211 0 101.712-1.715L9.602 1.121a1.211 1.211 0 00-1.714.002L1.113 7.917a1.211 1.211 0 101.716 1.71l5.919-5.935 5.944 5.937z"></path>
 		</svg>
 	</div>
 </button>
 
+{JSON.stringify($$props)}
+
+
 <style>
-	.spaneco-scrollup {
+	.btn {
 		position: fixed;
-		right: 0;
+		right: var(--position);
 		bottom: 0;
 		z-index: 100;
 		padding: 10px 15px;
@@ -59,29 +70,30 @@
 		outline: none;
 		display: inline-block;
 		border: none;
-		border-radius: 4px;
 		cursor: pointer;
 		transition: transform .2s ease;
 	}
 
-	.spaneco-scrollup--visible {
+	.btn.is-visible {
 		transform: translateY(0);
 	}
 
-	.spaneco-scrollup__content {
+	.content {
 		display: flex;
 		align-items: center;
 		justify-content: center;
 		padding: 10px;
-		width: 18px;
-		height: 18px;
+		width: var(--width);
+		height: var(--width);
 		box-shadow: 0 1px 6px rgb(0 0 0 / 20%);
-		color: #fff;
-		background-color: #666666;
+		color: var(--clr-icon);
+		background-color: var(--clr-bg);
+		border-radius: var(--radius);
 	}
 
-	.spaneco-scrollup__icon {
+	.icon {
 		width: 15px;
 		height: 10px;
+		fill: currentColor;
 	}
 </style>
